9c1f9a1a01d5ddf0ac021fdb87f10ba8d67ab449
ACHP <bullrot-10@hotmail.fr>
2015-03-13 16:02:16 +0100

    td4-exo2_fini

--- src/com/example/td4/Activity2.java
+++ src/com/example/td4/Activity2.java
@@ -2,14 +2,19 @@ package com.example.td4;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Activity2 extends Activity {
 	
-	Integer[] tab= {1,2,3,4,5,6};
-	String[] tp= {"Leibniz","Descartes","Euler"};
+	public Integer[] tab= {1,2,3,4,5,6};
+	public TextView tv;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 	
@@ -24,9 +29,25 @@ public class Activity2 extends Activity {
 		ArrayAdapter<Integer>  dataAdapter = new ArrayAdapter<Integer>(this,android.R.layout.simple_spinner_item,tab );
 		Spinner sp = (Spinner)findViewById(R.id.spinner1);
 		
+		tv = (TextView)findViewById(R.id.textView1);
+		sp.setAdapter(dataAdapter);		
+		sp.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+
+			@Override
+			public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long arg3) {
+				tv.setText(Integer.toString(tab[position]));   
+				Toast.makeText(getApplicationContext(), Integer.toString((tab[position]*2)), Toast.LENGTH_SHORT).show();
+				   
+				
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+        });
 		
-		
-		
-		sp.setAdapter(dataAdapter);	                            
 	}
 }
