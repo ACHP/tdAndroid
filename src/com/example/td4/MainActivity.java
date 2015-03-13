c60c4bcd332e82182bdf46e1aad9fd315b5d0f2a
ACHP <bullrot-10@hotmail.fr>
2015-03-13 14:01:08 +0100

    td4-version1

--- src/com/example/td4/MainActivity.java
+++ src/com/example/td4/MainActivity.java
@@ -2,14 +2,27 @@ package com.example.td4;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
+	private String [] listStr;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		exo1();
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
+	
+	private void exo1(){
+		String[] tp= {"Leibniz","Descartes","Euler"};
+		ArrayAdapter<String> aa = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, tp);
+		ListView lv = (ListView)findViewById(R.id.listView1);
+		lv.
+		
+		}
+	
+	
 }
